9bf9780fb9487786c4fb55e76d26728a9c97195f
Simon Peters <134172865+spbureau@users.noreply.github.com>
2023-09-10 22:25:40 +0000

    Updated firebase_firestore.dart

--- firebase_firestore.dart
+++ firebase_firestore.dart
@@ -1,2 +1,22 @@
 // Connecting Flutter App to Firebase Firestore
 // Sample codes - Raw
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+//main
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MaterialApp(
+title: "Flutter Firestore",
+home: My app(),
+));
+}
+
+// MyApp

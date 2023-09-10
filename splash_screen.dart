41d32daee068637c5a7091374519810165373db5
Simon Peters <134172865+spbureau@users.noreply.github.com>
2023-09-10 22:08:58 +0000

    Created splash_screen.dart

---
+++ splash_screen.dart
@@ -0,0 +1,5 @@
+// Flutter Splash Screen sample code- 2 Method:
+// In App and Native Splash Screens
+
+// Starting with Native Splash
+// That's pubspec.yaml configurations for splash screen outside of app

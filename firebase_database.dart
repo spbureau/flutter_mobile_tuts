73f6d5522ed2e63b05fc88048e058d1e6e870426
Simon Peters <134172865+spbureau@users.noreply.github.com>
2023-09-10 22:22:24 +0000

    Update firebase_database.dart

--- firebase_database.dart
+++ firebase_database.dart
@@ -5,3 +5,31 @@
 // import 'image picker'
 // import 'firebase firestore, database
 
+import 'dart:async';
+import 'dart:io' as io;
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'firebase_options.dart';
+import 'save_as/save_as.dart';
+
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+
+ WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MaterialApp(
+  title: "Image Picker Tutorials"
+  home: ImagePicker();
+));
+}
+
+// 
